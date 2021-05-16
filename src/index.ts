5d111e8dcf920ecb1fb778755405530be33811fd
Mark Wotton <mwotton@gmail.com>
2021-05-16 11:04:54 -0400

    fix path for remote

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ async function load() {
         from: "inline",
         config: {
           serverMode: "full",
-          url: "/db.sqlite3.000",
+          url: "rentalhub/db.sqlite3.000",
           requestChunkSize: 4096,
         },
       },
