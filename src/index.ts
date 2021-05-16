5f78d1554ae0745c508fa5521809645a7d3f7c13
Mark Wotton <mwotton@gmail.com>
2021-05-16 11:00:53 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { createDbWorker } from "sql.js-httpvfs";
+
+const workerUrl = new URL(
+  "sql.js-httpvfs/dist/sqlite.worker.js",
+  import.meta.url
+);
+const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
+
+async function load() {
+  const worker = await createDbWorker(
+    [
+      {
+        from: "inline",
+        config: {
+          serverMode: "full",
+          url: "/db.sqlite3.000",
+          requestChunkSize: 4096,
+        },
+      },
+    ],
+    workerUrl.toString(),
+    wasmUrl.toString()
+  );
+
+  const result = await worker.db.query(`select * from posts`);
+
+  document.body.textContent = JSON.stringify(result);
+}
+
+load();
